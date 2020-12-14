9ead8d9311afdbf0da8f278b9442624e7b9aa5b4
Maximilian Friedmann <max@smallstack.com>
2020-12-14 11:49:07 +0100

    Add gitlab yml

---
+++ index.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "plugins": ["@typescript-eslint"],
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  "rules": {
+    "@typescript-eslint/explicit-member-accessibility": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-parameter-properties": "off",
+    "@typescript-eslint/no-inferrable-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-floating-promises": 1,
+    "@typescript-eslint/no-unused-vars": ["warn", { "args": "none" }],
+    "no-warning-comments": ["warn", { "terms": ["fixme"] }],
+    "semi": "warn",
+    "quotes": "error",
+    "no-unreachable": "warn",
+    "object-shorthand": "warn",
+    "no-case-declarations": "warn",
+    "no-useless-escape": "warn",
+    "no-prototype-builtins": "warn"
+  }
+}
