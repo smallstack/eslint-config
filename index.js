eae43771f7cde7913d605f23e29e92e0e7f14e3c
Max Friedmann <max@smallstack.com>
2021-07-30 09:16:12 +0200

    Add few more rules

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ module.exports = {
     "@typescript-eslint/no-parameter-properties": "off",
     "@typescript-eslint/no-inferrable-types": "off",
     "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/no-floating-promises": 1,
+    "@typescript-eslint/no-floating-promises": "error",
     "@typescript-eslint/no-unused-vars": ["error", { "args": "none" }],
     "no-warning-comments": ["warn", { "terms": ["fixme"] }],
     "semi": "warn",
@@ -21,6 +21,18 @@ module.exports = {
     "object-shorthand": "warn",
     "no-case-declarations": "warn",
     "no-useless-escape": "warn",
-    "no-prototype-builtins": "warn"
+    "no-prototype-builtins": "warn",
+    "indent": ["error", 2],
+    "object-curly-spacing": ["error", "always"],
+    "array-bracket-spacing": ["error", "always"],
+    "computed-property-spacing": ["error", "always"],
+    "no-multi-spaces": ["error", "always"],
+    "brace-style": ["error", "stroustrup", { "allowSingleLine": true }],
+    "comma-spacing": ["error", { "before": false, "after": true }],
+    "comma-dangle": ["error", "never"],
+    "func-call-spacing": ["error", "never"],
+    "max-lines-per-function": ["warn", { max: 100, skipBlankLines: true, skipComments: true }],
+    "complexity": ["warn"],
+    "no-trailing-spaces": ["error"]
   }
 }
