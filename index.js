1581d17a037946f83fee376591fee90bb0ed3cfe
Max Friedmann <max@smallstack.com>
2023-03-22 10:27:22 +0100

    Use warn level for moment js eslint plugin

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 module.exports = {
-  "plugins": ["@typescript-eslint", "rxjs", "@angular-eslint", "you-dont-need-momentjs"],
-  "extends": [
+  plugins: [
+    "@typescript-eslint",
+    "rxjs",
+    "@angular-eslint",
+    "you-dont-need-momentjs",
+  ],
+  extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:you-dont-need-momentjs/recommended"
   ],
-  "rules": {
-    "@typescript-eslint/explicit-member-accessibility": ["warn", {
-      "overrides": {
-        "constructors": "off"
+  rules: {
+    "@typescript-eslint/explicit-member-accessibility": [
+      "warn",
+      {
+        overrides: {
+          constructors: "off",
+        },
+        accessibility: "explicit",
       },
-      "accessibility": "explicit"
-    }],
+    ],
     "@typescript-eslint/explicit-module-boundary-types": "warn",
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-parameter-properties": "off",
     "@typescript-eslint/no-inferrable-types": "off",
     "@typescript-eslint/no-explicit-any": "warn",
     "@typescript-eslint/no-floating-promises": "error",
-    "@typescript-eslint/no-unused-vars": ["error", { "args": "none" }],
+    "@typescript-eslint/no-unused-vars": ["error", { args: "none" }],
     "@angular-eslint/prefer-on-push-component-change-detection": "error",
     "@angular-eslint/no-lifecycle-call": "error",
     "@angular-eslint/prefer-output-readonly": "warn",
@@ -28,31 +35,37 @@ module.exports = {
     "rxjs/no-ignored-subscription": "warn",
     "rxjs/finnish": "warn",
     "no-console": "warn",
-    "no-warning-comments": ["warn", { "terms": ["fixme"] }],
-    "semi": "warn",
-    "quotes": "error",
+    "no-warning-comments": ["warn", { terms: ["fixme"] }],
+    semi: "warn",
+    quotes: "error",
     "no-unreachable": "warn",
     "object-shorthand": "warn",
     "no-case-declarations": "warn",
     "no-useless-escape": "warn",
     "no-prototype-builtins": "warn",
-    "indent": ["error", 2, { "SwitchCase": 1 }],
+    indent: ["error", 2, { SwitchCase: 1 }],
     "no-multi-spaces": ["error"],
-    "brace-style": ["error", "stroustrup", { "allowSingleLine": true }],
-    "comma-spacing": ["error", { "before": false, "after": true }],
+    "brace-style": ["error", "stroustrup", { allowSingleLine: true }],
+    "comma-spacing": ["error", { before: false, after: true }],
     "comma-dangle": ["error", "never"],
     "func-call-spacing": ["error", "never"],
-    "max-lines-per-function": ["warn", { max: 100, skipBlankLines: true, skipComments: true }],
-    "complexity": ["warn"],
-    "no-trailing-spaces": ["error"]
+    "max-lines-per-function": [
+      "warn",
+      { max: 100, skipBlankLines: true, skipComments: true },
+    ],
+    complexity: ["warn"],
+    "no-trailing-spaces": ["error"],
+    "you-dont-need-momentjs/no-import-moment": "warn",
+    "you-dont-need-momentjs/no-moment-constructor": "warn",
+    "you-dont-need-momentjs/format": "warn",
   },
-  "overrides": [
+  overrides: [
     {
-      "files": ["*.spec.ts"],
-      "rules": {
+      files: ["*.spec.ts"],
+      rules: {
         "max-lines-per-function": "off",
-        "no-console": "off"
-      }
-    }
-  ]
-}
+        "no-console": "off",
+      },
+    },
+  ],
+};
